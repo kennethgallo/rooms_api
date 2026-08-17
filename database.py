e770921359beb0f2103d6e52be6087f21a5a92ef
Kenneth Gallo <kenneth.gallo@mscdirect.com>
2026-08-17 15:09:06 -0400

    more db setup

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
-from sqlmodel import create_engine
+from sqlmodel import SQLModel, create_engine
+
+import models
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url, connect_args={"chec'_same_thread": False})
+engine = create_engine(sqlite_url, connect_args={"chec'_same_thread": False}, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
